c23de2680ea70a371fea03807f9123b75b7b10e5
Akila Shashith <133826637+gaasganegoda@users.noreply.github.com>
2024-07-02 05:15:42 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-
-// app.js
-const url = 'https://public.tableau.com/app/profile/akila.shashith/viz/HR_Analytics_Dashboard_17183638561070/HRAnalyticsDashboard'; // Replace with your Tableau dashboard link
-const vizContainer = document.getElementById('vizContainer');
-const viz = new tableau.Viz(vizContainer, url);
